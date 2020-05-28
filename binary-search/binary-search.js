48f49f09ac6592a0874c77037fac4d22ef7bc349
Kate Norton <katernorton@outlook.com>
2020-05-28 16:42:31 -0400

    binary search complete w/ extra credit

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,8 +1,12 @@
 'use strict';
 
 // Complete this algo
-const binarySearch = (array, target) => {
-	
+const binarySearch = (array, target, startidx = 0, endidx = array.length - 1) => {
+	if(startidx > endidx) return false
+	let middleishIndex = startidx + Math.floor(((endidx - startidx) / 2))
+	if(array[middleishIndex] == target) return true
+	if(array[middleishIndex] > target) return binarySearch(array, target, startidx, middleishIndex - 1) //go lower
+	if(array[middleishIndex] < target) return binarySearch(array, target, middleishIndex + 1, endidx) //go higher
 };
 
 /*
